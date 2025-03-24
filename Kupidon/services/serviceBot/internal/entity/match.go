62467ae358fad89b0dddaf167384f38a86816686
iviv660 <i_v_i_v@vk.ru>
2025-03-24 15:17:06 +0900

    Delete Kupidon directory

--- Kupidon/services/serviceBot/internal/entity/match.go
+++
@@ -1,7 +0,0 @@
-package entity
-
-type Match struct {
-	FromUserID int64
-	ToUserID   int64
-	CreatedAt  int64
-}
--- Kupidon/services/serviceUser/internal/entity/user_filter.go
+++
@@ -1,8 +0,0 @@
-package entity
-
-type UserFilter struct {
-	MinAge *int   `json:"min_age,omitempty"`
-	MaxAge *int   `json:"max_age,omitempty"`
-	City   string `json:"city,omitempty"`
-	Gender string `json:"gender,omitempty"`
-}
