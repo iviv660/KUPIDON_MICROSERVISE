a881704353828b52248fa09cbe6225121f7779fe
iviv660 <i_v_i_v@vk.ru>
2025-03-24 15:25:00 +0900

    Delete services directory

--- services/serviceMatch/migrations/000001_create_match_table.up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE likes (
-    id SERIAL PRIMARY KEY,       -- Уникальный идентификатор для каждой записи
-    from_user_id BIGINT NOT NULL, -- ID пользователя, который поставил лайк
-    to_user_id BIGINT NOT NULL
-);
